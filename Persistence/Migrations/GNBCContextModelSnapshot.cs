65ae167a18d1256ec80f5eeabd0f3e7d3c9c5b88
Baraiboapex <31634799+Baraiboapex@users.noreply.github.com>
2021-07-05 10:17:26 -0400

    space in migrations folder

--- Persistence/Migrations/GNBCContextModelSnapshot.cs
+++ Persistence/Migrations/GNBCContextModelSnapshot.cs
@@ -6,6 +6,7 @@ using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Persistence;
 
+
 namespace Persistence.Migrations
 {
     [DbContext(typeof(GNBCContext))]
